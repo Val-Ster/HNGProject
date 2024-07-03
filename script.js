b6e031320f255dd3823c78b052468bed3bb08e20
Val-ster <vster310@gmail.com>
2024-07-03 22:55:25 +0100

    A single-page website to display my goals for the next 2 years in tech

---
+++ script.js
@@ -0,0 +1,19 @@
+function updateTime() {
+  const now = new Date();
+  const utcTime = now.toUTCString();
+  document.getElementById("utcTime").innerText = utcTime;
+}
+setInterval(updateTime, 1000);
+updateTime(); // Initial call to display time immediately
+
+const daysOfWeek = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+document.getElementById("currentDay").innerText =
+  daysOfWeek[new Date().getDay()];
